f2672b3060e669d25a17aad14d7a04c509cb787d
Vianney Baskinas <vianbask07@yahoo.com>
2023-05-26 00:46:08 -0700

    One last minor issue

--- js/player.js
+++ js/player.js
@@ -1,7 +1,22 @@
 const $ = (id) => document.getElementById(id)
 
-class Jukebox {
+class Album {
+    constructor(artist, title) {
+        this.artist = artist
+        this.title = title
+        this.played = 0
+    }
+    play = function() {
+        this.played ++    
+    }
+
+    display = function(fav) {
+        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
+    }
+}
+class Jukebox extends Album{
     constructor(albums) {
+        super()
         this.albums = []
     }
     addAlbum = function(album) {
@@ -20,59 +35,51 @@ class Jukebox {
     }
 }
 
-class Album {
-    constructor(artist, title) {
-        this.artist = artist
-        this.title = title
-        this.played = 0
-    }
-    play = function() {
-        this.played += 1
-        
-    }
-
-    display = function() {
-        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
-    }
-}
-
 let songlist = [['Operation Ivy', 'Energy'], ['Blink 182', 'Dude Ranch'], ['New Found Glory', 'Sticks and Stones']]
-
 let jukebox = document.querySelector('#jukebox') 
 let songList = document.querySelector('#songList')
-var jbox = new Jukebox()
-// let album = []
+let album = []
 
-function album1() {
-    let album =[]
+function album1() {   
     for (let i = 0; i < songlist.length; i++ ) {
         let option = document.createElement('option')     
         album[i] = new Album(songlist[i][0], songlist[i][1])
-
         songList.innerHTML +=
         `<option>${album[i].artist}, ${album[i].title}</option>  
-        `
-        songList.appendChild(option)       
-    } 
+        `       
+    }   
 }
 
+let jbox = new Jukebox()
 window.addEventListener('load', () => {
     album1()
 })
 
-btn.addEventListener('click',()=> {        
+btn.addEventListener('click',()=> { 
     let x = $('songList').value
-    x = x.split(/\,/)
-    const h = new Album (x[0], x[1])
-    jbox.addAlbum(h)
-    console.log(jbox.albums)
-    h.play() 
-    $('songList').value = ' '     
-})
+    
+    if(x == 'Operation Ivy, Energy') {
+        album[0].play()
+        jbox.addAlbum(album[0])        
+    }
+    
+    if(x == 'Blink 182, Dude Ranch') {
+        album[1].play()
+        jbox.addAlbum(album[1])
+    }
+    if(x == 'New Found Glory, Sticks and Stones') {
+        album[2].play()
+        jbox.addAlbum(album[2])
+    }  
+}) 
 
-favAlbum.addEventListener('click',()=> {
+favAlbum.addEventListener('click', () => {
+    console.log(jbox.album)
     alert(jbox.favoriteAlbum())
 })
+
+
+
 
 
 
